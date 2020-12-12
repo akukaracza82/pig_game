5bbc6f822dc9b327f89e48cf62028be91ad9bfd8
Artur Mrozinski <amrozinski@gmail.com>
2020-12-12 21:13:29 +0000

    added dice roll functionality

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 'use strict';
 
 const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('#score--1');
-const dice = document.querySelector('.dice');
+const score1El = document.getElementById('score--1');
+const diceEL = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
-dice.classList.add('hidden');
+diceEL.classList.add('hidden');
+
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  diceEL.classList.remove('hidden');
+  diceEL.src = `dice-${dice}.png`;
+});
