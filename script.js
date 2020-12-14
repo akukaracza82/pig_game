958e97361a8eacf61d6c623f52b1ef14be188efc
Artur Mrozinski <amrozinski@gmail.com>
2020-12-14 13:54:42 +0000

    added player switch functionality

--- script.js
+++ script.js
@@ -2,10 +2,17 @@
 
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const currentScorePl1 = document.getElementById('current--0');
+const currentScorePl2 = document.getElementById('current--1');
 const diceEL = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+
+let currentScore = 0;
+let activePlayer = 0;
 
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -16,4 +23,20 @@ btnRoll.addEventListener('click', function () {
 
   diceEL.classList.remove('hidden');
   diceEL.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+
+    // toggle makes sure that one class gets hidden, when the other gets activated.
+    // Player 0 looses active, while player 1 gets it
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
 });
